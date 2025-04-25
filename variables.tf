f6d9ddfba672b76b4c901d357cced5c12172dafe
VETRI9876 <113588034+VETRI9876@users.noreply.github.com>
2025-04-25 18:19:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "AWS_ACCESS_KEY_ID" {
-  type = string
   description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
   description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
 }
